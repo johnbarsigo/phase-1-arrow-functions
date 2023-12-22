69fb9fd3fb8f410f5206e5b9f1a2654f76fd349d
johnbarsigo <john.barsigo@student.moringaschool.com>
2023-12-22 22:06:57 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function (param1, param2){
+    param1 = 2000;
+    param2 = 100;
+    return param1/param2;
+}
+
+//console.log (divide());
+
+const square = num1 => num1 * num1;
+
+//console.log(square(7));
+
+const add = (number1, number2) => number1 + number2;
+
+//console.log ( add(27, 53) );
